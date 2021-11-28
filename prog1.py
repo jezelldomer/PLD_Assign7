f67cd37b9c6686cae0d7644763dcca50d0caa5c3
Jezell <jzlldomer@gmail.com>
2021-11-28 16:10:32 +0800

    Adding colors and editing some codes

--- prog1.py
+++ prog1.py
@@ -1,7 +1,7 @@
 # Create a program that ask for a sentence as input
 # Display the number of words, vowels and consonants in the input 
 
-count_ = input("Enter your sentence here: ")
+count_ = input("\n\033[1;37;40mEnter the sentence you want In the space provided:\n»•»  \033[1;36;40m ")
 
 def _words(num_words = 1, num_vwls = 0, num_cons = 0):
     loop_str = count_.upper()
@@ -15,8 +15,9 @@ def _words(num_words = 1, num_vwls = 0, num_cons = 0):
     return num_words, num_vwls, num_cons
 
 words, vwls, cons = _words()
-print (count_)
-print ("Output: ")
+print ("\n╔═══════════╗")
+print ("\033[1;32;40m   Output: ")
+print ("╚═══════════╝")
 
-Statement = '\nWords: {} \nVowels: {} \nConsonant: {}\n'.format(words, vwls, cons)
+Statement = '\n\033[1;31;40m»•»   Words: {} \n\033[1;33;40m»•»   Vowels: {} \n\033[1;35;40m»•»   Consonant: {}\n'.format(words, vwls, cons)
 print(Statement)
